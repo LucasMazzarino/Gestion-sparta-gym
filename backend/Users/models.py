a674fbdd71eb3168f785cfbbecf83dad1e3b74f4
Lucas Mazzarino <Mazzanga1212@gmail.com>
2022-07-23 21:27:39 -0300

    Master

---
+++ backend/Users/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Usuarios(models.Model):
+
+  class PosteoUsuarios (models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()
+
+  nombre = models.CharField(max_length=250)
+  pellido = models.CharField(max_length=250)
+  cedula = models.IntegerField(unique=True, null=False, blank=False)
+  email = models.EmailField(max_length=250)
+  direccion = models.CharField(max_length=250)
+  esta_activo = models.BooleanField(default=True)
+
+  objectos = models.Manager() #default manager
+  posteoobjetos = PosteoUsuarios() #custom manager
+  
+  # class Meta:
+  #   orden = ('-apellido')
+  
+  def __str__(self):
+      return self.nombre
+  
