fd588a305334d1bcdddfdf75c183811c70744b0c
Santiago Silva <57465367+Tiaghovski@users.noreply.github.com>
2022-08-05 08:55:56 -0300

    configuración postgresql

--- backend/Main/settings.py
+++ backend/Main/settings.py
@@ -126,8 +126,12 @@ WSGI_APPLICATION = 'Main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'spartagym' ,
+        'USER' : 'postgres' ,
+        'PASSWORD' : 'Zandalaris' , 
+        'HOST' : 'localhost' ,
+        'PORT' : 5432
     }
 }
 
@@ -173,7 +177,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '../frontend/build/static')
+    os.path.join(BASE_DIR, './frontend/build/static')
 ]
 
 AUTH_USER_MODEL = 'Users.Usuarios'
