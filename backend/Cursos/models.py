35f81892892544c7658327861521dc015067b4f9
Lucas Mazzarino <Mazzanga1212@gmail.com>
2022-09-03 16:43:20 -0300

    probando Inlines cursosAsistencia

--- backend/Cursos/models.py
+++ backend/Cursos/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
+from Users.models import Usuarios
 
 class Horario(models.Model):
   id = models.AutoField(primary_key=True)
   horaInicio = models.TimeField()
   horaFin = models.TimeField()
-  cuposLibres = models.PositiveSmallIntegerField(default=40)
 
   class Meta:
    verbose_name = 'Horario'
@@ -18,15 +17,16 @@ class Horario(models.Model):
 
 class Cursos(models.Model):
   id = models.AutoField(primary_key=True)
+  usuarios = models.ManyToManyField(Usuarios)
   nombre = models.CharField(max_length=250)
   costo = models.PositiveSmallIntegerField(default=0)
   descripcion = models.TextField(max_length=250)  
   state = models.BooleanField('Estado',default = True)
-  horario = models.ManyToManyField(Horario) 
+  horarios = models.ManyToManyField(Horario, through='CursoHorario') 
   
   @property
   def ganancia(self):
-    ganancia = self.usuarios_set.filter(pago_cuota=True).count()*self.costo
+    ganancia = self.usuarios.filter(pago_cuota=True).count()*self.costo
     return ganancia
 
 
@@ -34,6 +34,21 @@ class Cursos(models.Model):
     txt = "{0} (Costo: $ {1} por mes)"
     return txt.format(self.nombre, self.costo)
 
+
+class CursoHorario(models.Model):
+  horario = models.ForeignKey(Horario, on_delete=models.CASCADE)
+  curso =models.ForeignKey(Cursos, on_delete=models.CASCADE)
+  usuario = models.ManyToManyField(Usuarios, through='AsistenciaCursoUsuario')
+  cupo = models.PositiveSmallIntegerField(default=40)
+  dia = models.DateField()
+
+class AsistenciaCursoUsuario(models.Model):
+  usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+  curso_horario = models.ForeignKey(CursoHorario, on_delete=models.CASCADE)
+  asistencia = models.BooleanField(default=False)
+  
+  def get_usuarios(self, obj):
+        return "\n".join([u.usuario for u in obj.usuario.all()])
 
 
 
