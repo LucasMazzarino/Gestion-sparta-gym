ca531bb081611fd1ba51aee9b2c5359fb277af1e
Santiago Silva <57465367+Tiaghovski@users.noreply.github.com>
2022-09-08 23:28:12 -0300

    se arregla footer, se arregla section 3

--- frontend/src/componentes/Footer.js
+++ frontend/src/componentes/Footer.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Facebook from '../imagenes/Facebook.png';
+import Instagram from '../imagenes/Instagram.png';
+import Twitter from '../imagenes/Twitter.png';
+import Whatsapp from '../imagenes/Whatsapp.png';
 
 function Footer() {
   return (
@@ -8,16 +12,17 @@ function Footer() {
               <div className='row'>
                 <div className='col-12 col-md-4 d-flex aling-items-center justify-content-center'>               
                     <p className='font-weight-bold mb-2'>¡Síguenos en nuestras redes sociales!<br/>                      
-                        <i className="bi bi-facebook"></i>
-                        <i className="bi bi-instagram"></i>
-                        <i className="bi bi-twitter"></i></p>              
+                        <img src={Facebook} height='40' width='40'/>
+                        <img src={Instagram} height='40' width='40'/>
+                        <img src={Twitter} height='40' width='40'/>               
+                    </p>           
                 </div>
                 <div className='col-12 col-md-4 d-flex aling-items-center justify-content-center'>
                     <p id='Derechos'>® 2022 SPARTA GYM All rights reserved <br/>
                     Politica de privacidad - Designed by MazzSilv</p>
                 </div>
                 <div className='col-12 col-md-4 d-flex aling-items-center justify-content-center'>
-                  <button className="btn-flotante"><img src='https://assets.stickpng.com/images/5ae21cc526c97415d3213554.png' width='60' height='40'/>Comunicate con nosotros</button> 
+                  <button className="btn-flotante"><img src={Whatsapp} width='30' height='30'/>Comunicate con nosotros</button> 
                 </div>
               </div>
             </div>
--- frontend/src/componentes/SeccionTres.js
+++ frontend/src/componentes/SeccionTres.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import Cry from '../imagenes/Cry.jpg'
+import EntrenadorUno from '../imagenes/EntrenadorUno.png'
+import EntrenadorDos from '../imagenes/EntrenadorDos.jpg'
+import Image from 'react-bootstrap/Image'
 
 function SeccionTres(){
   return(
-    <div className='SeccionTres'> 
+    <section className='SeccionTres'> 
               <h1>Nuestros entrenadores</h1>
-              <p className='d-flex aling-items-center justify-content-center'>Veni a distrutar</p>
-    </div>
+              <p className='d-flex aling-items-center justify-content-center'>¡Te esperamos en el gimnasio que lo tiene todo! Aparatos nuevos de última generación, caminadores profesionales, bicis de spinning, escaladores, elípticas,  remos ergómetros</p> 
+              <p className='d-flex aling-items-center justify-content-center'>Entrenadores certificados siempre disponibles en sala, el mejor ambiente y un estricto protocolo sanitario para cuidarte. Vení a divertirte!</p>
+              <div className='container'>
+                <div className='row'>
+                  <div className='col-12 col-md-3'>
+                                          
+                  </div>
+                  <div className='col-12 col-md-3'>
+                        <div className='EntrenadorUno d-flex aling-items-center justify-content-center'>                        
+                          <Image src={EntrenadorUno} rounded width='300' height='500'/>  
+                          <p className='TextoUno'>Carlitos Tevez</p>                                                               
+                        </div>                        
+                  </div>
+                  <div className='col-12 col-md-3'>
+                        <div className='EntrenadorUno d-flex aling-items-center justify-content-center'>                        
+                          <Image src={EntrenadorDos} rounded width='300' height='500'/>  
+                          <p className='TextoDos'>Profe Bengoechea</p>                                                               
+                        </div>                        
+                  </div>
+                  <div className='col-12 col-md-3'>
+                                
+                  </div>                
+                </div>
+              </div>
+    </section>
   );
 }
 
