11e5f1bd45c3082bf8dc3d1f58f13408b4e5e5bf
Santiago Silva <57465367+Tiaghovski@users.noreply.github.com>
2022-09-12 20:52:48 -0300

    se agrego pag noticias y cursos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Navbar from './componentes/Navbar';
 import Footer from './componentes/Footer';
 import Inicio from './paginas/Inicio';
+import Cursos from './paginas/Cursos';
+import Noticias from './paginas/Noticias';
 import Contacto from './paginas/Contacto';
 import Galeria from './paginas/Galeria';
 
@@ -14,6 +16,8 @@ function App() {
         <Navbar/>
         <Routes>
            <Route index element={<Inicio/>}/>
+           <Route path='/Cursos' element={<Cursos/>}/>
+           <Route path='/Noticias' element={<Noticias/>}/>
            <Route path='/Contacto' element={<Contacto/>}/>
            <Route path='/Galeria' element={<Galeria/>}/>
         </Routes>
---
+++ frontend/src/paginas/Noticias.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Noticias(){
+    return(
+        <section className='Noticias'>
+            <h1>Noticias</h1>
+        </section>
+      
+    )
+}
+
+export default Noticias;
