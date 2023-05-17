47caefcb8afa791402c24fa5c88615d078b0236b
Lucas Mazzarino <Mazzanga1212@gmail.com>
2023-05-17 20:43:14 -0300

    .

--- backend/Main/settings.py
+++ backend/Main/settings.py
@@ -225,9 +225,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 if not DEBUG:
     DEFAULT_FROM_EMAIL= env('EMAIL_DEF')
     EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = env('EMAIL_HOST')
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-    EMAIL_PORT = env('EMAIL_PORT')
-    EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+    # EMAIL_HOST = env('EMAIL_HOST')
+    # EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    # EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    # EMAIL_PORT = env('EMAIL_PORT')
+    # EMAIL_USE_TLS = env('EMAIL_USE_TLS')
 
