b572e76dde02e4fc7bba7868669785189394aec2
Lucas Mazzarino <Mazzanga1212@gmail.com>
2023-05-20 01:11:44 -0300

    c

--- backend/Main/settings.py
+++ backend/Main/settings.py
@@ -8,7 +8,7 @@ import dj_database_url
 env = environ.Env()
 #leyendo env.File
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env()
 
 DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
