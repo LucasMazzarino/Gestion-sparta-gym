65a1153cd63755e896a05315f78ce7a8e65f86d0
Lucas Mazzarino <Mazzanga1212@gmail.com>
2023-05-23 23:28:34 -0300

    .

--- backend/Main/settings.py
+++ backend/Main/settings.py
@@ -165,8 +165,8 @@ USE_TZ = True
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
