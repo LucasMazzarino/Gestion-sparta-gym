f02d2009b25884392d44d9693f6827710f43eb4f
Lucas Mazzarino <Mazzanga1212@gmail.com>
2023-06-06 02:28:20 -0300

    a

--- backend/Main/settings.py
+++ backend/Main/settings.py
@@ -89,26 +89,34 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    "http://", "https://gestion-sparta-gym-production.up.railway.app"
+    "http://",
+    "https://", 
+    "https://gestion-sparta-gym-production.up.railway.app"
     'http://localhost:3000',
     'http://localhost:8000',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://", "https://gestion-sparta-gym-production.up.railway.app",
+    "http://",
+    "https://", 
+    "https://gestion-sparta-gym-production.up.railway.app"
     'http://localhost:3000',
     'http://localhost:8000',
 ]
 
 if not DEBUG:
     CORS_ORIGIN_WHITELIST = [
-        "http://", "https://gestion-sparta-gym-production.up.railway.app"
+        "http://",
+        "https://", 
+        "https://gestion-sparta-gym-production.up.railway.app"
         'https://spartagym.com',
         'https://admin.spartagym.com',
      ]
 
     CSRF_TRUSTED_ORIGINS[
-        "http://", "https://gestion-sparta-gym-production.up.railway.app"
+        "http://",
+        "https://", 
+        "https://gestion-sparta-gym-production.up.railway.app"
         'https://spartagym.com',
         'https://admin.spartagym.com',
      ]
